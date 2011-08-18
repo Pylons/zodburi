67c1ace33b7f60c5ec35631db5d2266ca390bdc9
Chris Rossi <chris@archimedeanco.com>
2011-08-18 13:55:00 -0400

    Initial skeleton.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except:
+    README = ''
+    CHANGES = ''
+
+requires = ['ZODB3']
+
+setup(name='zodburi',
+      version='0.1a1',
+      description=('Constructs ZODB storage instances from URIs.'),
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: Repoze Public License",
+        ],
+      keywords='zodb zodbconn',
+      author="Chris Rossi",
+      author_email="pylons-discuss@googlegroups.com",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require = requires,
+      install_requires = requires,
+      test_suite="zodburi",
+      )
+
