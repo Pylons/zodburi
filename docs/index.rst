00efdffeca89177917105b1f8b0ec65dc0556644
Tres Seaver <tseaver@palladion.com>
2013-05-02 14:47:52 -0400

    Drop support for Python 2.5.

--- docs/index.rst
+++ docs/index.rst
@@ -7,7 +7,7 @@ Overview
 A library which parses URIs and converts them to ZODB storage objects and
 database arguments.
 
-It will run under CPython 2.5, 2.6, and 2.7.  It will not run under PyPy or
+It will run under CPython 2.6, and 2.7.  It will not run under PyPy or
 Jython.  It requires ZODB >= 3.10.0.
 
 Installation
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from setuptools import setup
 from setuptools import find_packages
@@ -7,25 +6,29 @@ from setuptools import find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
 try:
-    README = open(os.path.join(here, 'README.rst')).read()
-    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
 except:
     README = ''
+
+try:
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except:
     CHANGES = ''
 
 requires = ['ZODB3']
-if sys.version_info[:2] < (2, 6):
-    requires.insert(0, 'zope.interface>=3.6.0,<4.0dev')
-    requires.insert(0, 'transaction<1.2')
 tests_require = requires + ['mock']
 
 setup(name='zodburi',
-      version='1.1',
+      version='1.2dev',
       description=('Constructs ZODB storage instances from URIs.'),
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "License :: Repoze Public License",
         ],
       keywords='zodb zodbconn',
