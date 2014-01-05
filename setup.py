181ffc5df3074d289a49e680962703cc557ec798
Tres Seaver <tseaver@palladion.com>
2014-01-05 12:25:03 -0500

    Prep 2.0 release.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ testing_extras = tests_require + ['nose', 'coverage']
 docs_extras = tests_require + ['Sphinx']
 
 setup(name='zodburi',
-      version='2.0b1',
+      version='2.0',
       description=('Construct ZODB storage instances from URIs.'),
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
