c03a7433cf2c913629f824ff807eb8d337c4531e
Jim Fulton <jim@jimfulton.info>
2017-04-13 12:16:12 -0400

    Add support for additional database configuration parameters: pool_timeout, cache_size_bytes, historical_pool_size, historical_cache_size, historical_cache_size_bytes, historical_timeout, and large_record_size

--- zodburi/tests/__init__.py
+++ zodburi/tests/__init__.py
@@ -17,16 +17,22 @@ class TestResolveURI(unittest.TestCase):
 
     @mock.patch('zodburi.resolvers.MappingStorage')
     def test_it_with_dbkw(self, MappingStorage):
-        from zodburi import resolve_uri
-        factory, dbkw = resolve_uri(
-            'memory://test?connection_cache_size=1&connection_pool_size=2&'
-            'database_name=dbname')
+        from zodburi import resolve_uri, connection_parameters, parameters
+        uri = 'memory://test?database_name=dbname'
+        for i, parameter in enumerate(connection_parameters):
+            uri += '&connection_%s=%d' % (parameter, i)
+            if parameter == 'cache_size_bytes':
+                uri += 'MB'
+        factory, dbkw = resolve_uri(uri)
         factory()
         MappingStorage.assert_called_once_with('test')
-        self.assertEqual(dbkw, {
-            'cache_size': 1,
-            'pool_size': 2,
-            'database_name': 'dbname'})
+        expect = dict(database_name='dbname')
+        for i, parameter in enumerate(connection_parameters):
+            parameter = 'connection_' + parameter
+            expect[parameters[parameter]] = i
+            if parameter == 'connection_cache_size_bytes':
+                expect[parameters[parameter]] *= 1<<20
+        self.assertEqual(dbkw, expect)
 
     def test_it_cant_resolve(self):
         from zodburi import resolve_uri
